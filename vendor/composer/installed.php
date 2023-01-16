fc9df9a11dac11a2f2e7acafee14cad0d80fcd91
Thomas Skerbis <skerbis@klxm.de>
2023-01-16 14:37:47 +0100

    add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f5ad6a03294941c6ec33bac57d39383ce43c0ef8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f5ad6a03294941c6ec33bac57d39383ce43c0ef8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'ricardoper/twitteroauth' => array(
+            'pretty_version' => 'v2.1.6',
+            'version' => '2.1.6.0',
+            'reference' => 'cbdd43db781f30a4f291b15b90ab08a30108d5d6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ricardoper/twitteroauth',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
